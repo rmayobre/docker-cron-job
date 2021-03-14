ab56cc61902a4615cfb0eb1e549b445ee56066c8
Ryan Mayobre <rbmayobre4@gmail.com>
2021-03-14 14:47:26 -0400

    Finished docker compose file

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,7 +6,7 @@ ls /var/spool/cron/crontabs/
 # cat /var/spool/cron/crontabs/*.cron | crontab - && crond -f -L /dev/stdout
 cat /var/spool/cron/crontabs/*.cron | crontab - && crond -b -L /var/log/cron/cron.log
 
-echo "Press any key to continue"
+echo "Press CTRL-C to exit"
 while [ true ] ; do
 read -t 3 -n 1
 if [ $? = 0 ] ; then
